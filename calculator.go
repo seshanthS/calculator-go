0e10e8bfa38fafc56ce118189dd14649ecbeded0
rajeshkumarrobert <rajeshkumar.robert@gmail.com>
2018-08-31 23:38:35 +0530

    added subtraction

---
+++ calculator.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+func main(){
+    subtraction()
+	}
+    func subtraction(){
+	var a,b int
+	
+	fmt.Printf("enter two numbers"\n)
+	fmt.Scanf ("%d,%d",&a,&b)
+	sub:=a-b
+	fmt.Printf("the result is %d",sub)
+	}
+}
