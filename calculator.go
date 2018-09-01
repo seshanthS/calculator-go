d94f1a6fbc2c12be7c3ed39162c334b0313e300c
Seshanth <seshanth.s@outlook.com>
2018-09-01 09:56:36 +0530

    used gofmt

--- calculator.go
+++ calculator.go
@@ -1,22 +1,28 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+)
 
-func home(){
-	fmt.Printf(" 1.Addition \n 2.Subtraction \n 3.Average \n")
+func home() {
+// UI	
+fmt.Printf(" 1.Addition \n 2.Subtraction \n 3.Average \n")
 	fmt.Printf("Enter the number for the corresponding Operation : ")
+
+// Maps
 	test := make(map[int]string)
-	test[1]="Addition"
-	test[2]="subtraction"
-	test[3]="Average"
-	var choice int
-	fmt.Scanf("%d",&choice)
-	fmt.Printf("\n your choice is %s \n",test[choice])
+	test[1] = "Addition"
+	test[2] = "subtraction"
+	test[3] = "Average"
 	
+var choice int
+	fmt.Scanf("%d", &choice)
+	fmt.Printf("\n your choice is %s \n", test[choice])
+
 	//act on the choice
-	
-}	
-	
-func main(){
+
+}
+
+func main() {
 	home()
 }
