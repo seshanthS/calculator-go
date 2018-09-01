ad4dd5d6054d95c1b50cb29f90d54a3edc406483
Seshanth <seshanth.s@outlook.com>
2018-09-01 09:58:17 +0530

    gofmt calculator.go

--- calculator.go
+++ calculator.go
@@ -5,17 +5,17 @@ import (
 )
 
 func home() {
-// UI	
-fmt.Printf(" 1.Addition \n 2.Subtraction \n 3.Average \n")
+	// UI
+	fmt.Printf(" 1.Addition \n 2.Subtraction \n 3.Average \n")
 	fmt.Printf("Enter the number for the corresponding Operation : ")
 
-// Maps
+	// Maps
 	test := make(map[int]string)
 	test[1] = "Addition"
 	test[2] = "subtraction"
 	test[3] = "Average"
-	
-var choice int
+
+	var choice int
 	fmt.Scanf("%d", &choice)
 	fmt.Printf("\n your choice is %s \n", test[choice])
 
